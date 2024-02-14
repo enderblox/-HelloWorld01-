b8399025ebf056332656ba0f5cbdc16a587a8e0c
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-02-14 11:01:44 +0100

    whilelusv2

--- whilelus01.php
+++ whilelus01.php
@@ -1,4 +1,15 @@
 <?php
 
+$teller=1;
+$maximum=15;
+
+echo "Teller is voor de while lus".$teller."\n";
+
+while ($teller <= $maximum){
+    echo "Teller is nu" .$teller."\n";
+    $teller++;
+}
+
+echo "Teller is na de while lus".$teller."\n";
 
 ?>
