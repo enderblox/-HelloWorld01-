883c51ded311788dcd9d5fef33cd9e57b13b4b53
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-02-15 11:08:09 +0100

    ifkeuze01v3

--- ifkeuze01.php
+++ ifkeuze01.php
@@ -5,14 +5,14 @@ $leeftijd=readline("Geef je leeftijd: ");
 
 echo "Hello $naam \n";
 
-if($leeftijd < 22){
-    echo "Jij bent jonger dan 22 jaar.\n";
+if($leeftijd < 25){
+    echo "Jij bent jonger dan 25 jaar.\n";
 }
-if($leeftijd > 22){
-    echo "Jij bent ouder dan 22 jaar.\n";
+if($leeftijd > 25){
+    echo "Jij bent ouder dan 25 jaar.\n";
 }
-if($leeftijd == 22){
-    echo "Jij bent precies 22 jaar.\n";
+if($leeftijd == 25){
+    echo "Jij bent precies 25 jaar.\n";
 }
 
 
