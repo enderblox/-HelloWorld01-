f73c2f696c5e8dae83b4c61f4cf0d4f0f86da261
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-02-15 12:03:42 +0100

    functiefor01v2

--- functiefor01.php
+++ functiefor01.php
@@ -1,4 +1,19 @@
 <?php
 
+function func(){
+    echo "= Deze code is in func() uitgevoerd.\n";
+}
+
+echo "- Deze code is voor de for-lus uitgevoerd.\n";
+
+for($counter=1;$counter<=3;$counter++){
+    echo "= Deze code is in de for-lus\n";
+    echo " maar voor de func() uitgevoerd.\n";
+    func();
+    echo "= Deze code is in de for-lus\n";
+    echo " maar na de func() uitgevoerd.\n";
+}
+
+echo "deze code is na de for-lus uitgevoerd.\n";
 
 ?>
