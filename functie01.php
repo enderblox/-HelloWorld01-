a9f07bb1ca10732446af0c4bbac5e6a70a9b37a7
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-02-15 11:52:07 +0100

    functie01v2

--- functie01.php
+++ functie01.php
@@ -1,6 +1,17 @@
 <?php
 
+function func(){
+    echo "+ Deze regel is in func uitgevoerd.\n";
+}
 
+echo "-- Deze code is voor func() uitgevoerd.\n";
 
+func();
+
+echo "== Deze regel is voor de func() uitgevoerd.\n";
+
+func();
+
+echo "-- Deze regel is na de func() uitgevoerd.\n";
 
 ?>
