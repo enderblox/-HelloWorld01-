cbe5e57a5b2b294a456a73d0e40a8495d72c716e
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-02-26 09:24:32 +0100

    quizv3

--- quiz.php
+++ quiz.php
@@ -1,7 +1,18 @@
 <?php
 
-$random=rand(1,100)
+$random=rand(1,100);
+$getal=$random;
+$antwoord=readline("Geef je antwoord: ");
 
+if($random==$getal){
+    echo "Jij hebt gewonnen!\n";
+}
 
+if($random<$getal){
+    echo "Fout geraden.\n";
+}
 
+if($random>$getal){
+    echo "Fout geraden.\n";
+}
 ?>
