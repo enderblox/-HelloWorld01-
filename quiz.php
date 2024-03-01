30c1bad6e71305fc26d73d3066b5488a060c417c
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-03-01 11:21:44 +0100

    quizv9

--- quiz.php
+++ quiz.php
@@ -3,23 +3,22 @@
 $random=rand(1,100);
 echo "$random";
 $getal = 0;
-
+$beurt = 1;
 
 while($random != $getal){
 
     $getal=readline("Geef je antwoord: ");
 
-    if($random<$getal){
+    if($random!=$getal){
         echo "Fout geraden.\n";
+        $beurt++;
     }
 
-    if($random>$getal){
-        echo "Fout geraden.\n";
-    }
 
 }
 
 
-echo "Geraden, gefeliciteerd";
-
+echo "Geraden, gefeliciteerd\n";
+$final = $beurt++;
+echo "Zo vaak moest je raden $final\n";
 ?>
