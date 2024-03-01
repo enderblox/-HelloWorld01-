e49edd64c2729c985d04697400f3db6f3d9b91a3
Student Koen Emmink <kemmink7436@student.landstede.nl>
2024-03-01 11:59:23 +0100

    quizv10

--- quiz.php
+++ quiz.php
@@ -9,16 +9,26 @@ while($random != $getal){
 
     $getal=readline("Geef je antwoord: ");
 
+    if($getal<1|| $getal>100|| $getal=="stop"){
+        echo "Dit is niet geldig, het spel stopt\n";
+        break;
+    }
+
     if($random!=$getal){
         echo "Fout geraden.\n";
         $beurt++;
     }
 
+    if($getal==$random){
+        echo "Geraden, gefeliciteerd\n";
+    $final = $beurt++;
+    echo "Zo vaak moest je raden $final\n";
+    break;
+    }
 
 }
 
 
-echo "Geraden, gefeliciteerd\n";
-$final = $beurt++;
-echo "Zo vaak moest je raden $final\n";
+
+
 ?>
